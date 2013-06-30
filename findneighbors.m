cbf6a4505c8c770e37e0b258b5214d44c1559c12
Scott Hardy <scott.the.hardy@gmail.com>
2013-06-30 00:18:13 -0600

    findneighbors function to find the valid neighbors of a square

---
+++ findneighbors.m
@@ -0,0 +1,16 @@
+function neighbors = findneighbors(h, m, n)
+    a = size(h, 1);
+    
+    % [up, right, down, left]
+    neighbors = [[m-1, n]; [m, n+1]; [m+1, n]; [m, n-1]];
+    
+    % find invalid coordinates and remove them
+    invalid = [];
+    for i=1:4
+        % if either coordinate is invalid
+        if (neighbors(i,1)<0 || neighbors(i,2)<0 || neighbors(i,1)>a || neighbors(i,2)>a)
+            invalid = [invalid, i];
+        end
+    end
+    neighbors(invalid, :) = [];
+end
